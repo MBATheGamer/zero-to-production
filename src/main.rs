9e1392484e07fbda1fbee6745e186d5cdd3417b6
MBATheGamer <mbathegamer@gmail.com>
2024-06-12 19:14:13 +0100

    Create web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer, Responder};
+
+async fn hello_world() -> impl Responder {
+    format!("Hello, World!")
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    HttpServer::new(|| App::new().route("/", web::get().to(hello_world)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
